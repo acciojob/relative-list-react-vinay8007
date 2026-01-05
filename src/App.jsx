a7a1b732a410211537e16eacb20bcee17ffba5b7
vinay8007 <161361880+vinay8007@users.noreply.github.com>
2026-01-06 00:53:03 +0530

    Create App component with RelativeList

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import RelativeList from "./RelativeList";
+const app = () => {
+  return(
+    <div>
+    <h1>Diwali RelativeList</h1>
+      <ReltiveList/>
+    </div>
+  );
+}
+export default App
