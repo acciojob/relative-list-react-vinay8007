d0715f8732aa0d7f6c5583bc5374207b79fbd785
vinay8007 <161361880+vinay8007@users.noreply.github.com>
2026-01-06 01:13:26 +0530

    Initialize main.jsx with React setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+import ReactDOM from "react-dom/clint";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
